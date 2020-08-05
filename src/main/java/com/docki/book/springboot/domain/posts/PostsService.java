f71a65fb49dd5231a5e59fa4c7003fb04d9b26f9
Jayden <pyc8992dev@gmail.com>
2020-08-05 23:46:19 +0900

    update & get api added

--- src/main/java/com/docki/book/springboot/domain/posts/PostsService.java
+++ src/main/java/com/docki/book/springboot/domain/posts/PostsService.java
@@ -1,6 +1,8 @@
 package com.docki.book.springboot.domain.posts;
 
+import com.docki.book.springboot.web.dto.PostsResponseDto;
 import com.docki.book.springboot.web.dto.PostsSaveRequestDto;
+import com.docki.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +16,18 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+        return new PostsResponseDto(entity);
     }
 }
