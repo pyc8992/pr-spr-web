6ea6d4d86c9552ff209b997b8097efdbdfa334a1
Jayden <pyc8992dev@gmail.com>
2020-08-09 16:13:32 +0900

    게시글 조회 기능 added

--- src/main/java/com/docki/book/springboot/domain/posts/PostsService.java
+++ src/main/java/com/docki/book/springboot/domain/posts/PostsService.java
@@ -1,12 +1,15 @@
 package com.docki.book.springboot.domain.posts;
 
+import com.docki.book.springboot.web.dto.PostsListResponseDto;
 import com.docki.book.springboot.web.dto.PostsResponseDto;
 import com.docki.book.springboot.web.dto.PostsSaveRequestDto;
 import com.docki.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,5 +32,11 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
